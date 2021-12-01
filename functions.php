cda5ed8ec3f7b005a41e5a7cade169deeb9c4049
“Elwood <me@elwoodberry.com>
2021-12-01 16:21:33 -0600

    Updated

--- functions.php
+++ functions.php
@@ -2,9 +2,9 @@
 
 /*
 ENQUEUE CHILD THEME STYLE SHEET
-'uiuxdallas'
+Run the css for child theme, 'uiuxdallas', last. 
 */
-add_action( 'wp_enqueue_scripts', 'enqueue_uiuxdallas_styles' );
+add_action( 'wp_enqueue_scripts', 'enqueue_uiuxdallas_styles', 11 );
 
 function enqueue_uiuxdallas_styles() {
 	wp_enqueue_style( 'child-style', get_stylesheet_uri());
